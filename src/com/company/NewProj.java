996d591729d83ef79380215e353e1e038920a625
katarina-luk <katarina.lukienko@gmail.com>
2020-09-20 10:54:24 +0300

    new branch commit 6

--- src/com/company/NewProj.java
+++ src/com/company/NewProj.java
@@ -4,5 +4,15 @@ public class NewProj {
     public static void main(String[] args) {
         System.out.println("Name");
         System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
     }
 }
