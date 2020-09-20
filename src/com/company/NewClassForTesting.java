49c53b8947322867eb0314511c4aa66f44999224
katarina-luk <katarina.lukienko@gmail.com>
2020-09-20 11:07:25 +0300

    new branch data 01

--- src/com/company/NewClassForTesting.java
+++ src/com/company/NewClassForTesting.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class NewClassForTesting {
     public static void main(String[] args) {
-        System.out.println("Helo my friend");
+        System.out.println("Hello my friend");
+        System.out.println("Hello another friend");
     }
 }
