ffeb9510b85af4b9fd9657c1ae44a7ef5a92c5d0
katarina-luk <katarina.lukienko@gmail.com>
2020-09-20 10:42:03 +0300

    3 commit 9898

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,6 @@ public class Main {
 
         System.out.println("Hi");
         System.out.println("Hi");
+        System.out.println("Hi");
     }
 }
--- src/com/company/NewProj.java
+++ src/com/company/NewProj.java
@@ -3,5 +3,6 @@ package com.company;
 public class NewProj {
     public static void main(String[] args) {
         System.out.println("Name");
+        System.out.println("Second");
     }
 }
